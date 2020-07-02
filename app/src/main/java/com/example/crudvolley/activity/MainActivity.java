8b9ab2fb93806a01c1d10240dd290c10432f78ec
lordrians <ferrianrp@gmail.com>
2020-07-02 17:51:25 +0700

    signin kelar simple

--- app/src/main/java/com/example/crudvolley/activity/MainActivity.java
+++ app/src/main/java/com/example/crudvolley/activity/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.crudvolley.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -35,8 +38,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        handlerAuth();
         init();
 
+    }
+
+    private void handlerAuth() {
+        Handler handler = new Handler();
+        handler.postAtTime(() -> {
+
+            SharedPreferences UserInfo = getSharedPreferences(Variable.SP_USER_FILE,0);
+            boolean isLoggedIn = UserInfo.getBoolean(Variable.IS_LOGGED, false);
+
+            if (!isLoggedIn){
+                startActivity(new Intent(getApplicationContext(), AuthActivity.class));
+                finish();
+            }
+
+        },0);
     }
 
     private void init() {
--- app/src/main/java/com/example/crudvolley/fragment/SignInFragment.java
+++ app/src/main/java/com/example/crudvolley/fragment/SignInFragment.java
@@ -1,5 +1,6 @@
 package com.example.crudvolley.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,22 +8,45 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.crudvolley.R;
 
 public class SignInFragment extends Fragment {
+    private View view;
+    private EditText etUsername, etPassword;
+    private Button btnLogin;
+    private TextView btnSignUp;
 
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+        view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        init();
+        return view;
+    }
+
+    private void init() {
+        etUsername = view.findViewById(R.id.et_username_signin);
+        etPassword = view.findViewById(R.id.et_password_signin);
+        btnLogin = view.findViewById(R.id.btn_login_signin);
+        btnSignUp = view.findViewById(R.id.tv_btn_signup_signin);
+
+        btnLogin.setOnClickListener(v -> {
+
+        });
+
+        btnSignUp.setOnClickListener(v -> {
+            getActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fl_auth_activity, new SignUpFragment())
+                    .commit();
+        });
+
     }
 }
